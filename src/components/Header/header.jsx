5b079bdb169f8ae1b691d55dd8dce948feea5bf6
Craig Neff <ifly12610@gmail.com>
2022-01-19 00:21:59 -0700

    more api react practice. setup

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./header.scss";
+
+export default function header() {
+  return (
+    <div>
+      <h1 className="love"> I like to build things</h1>
+    </div>
+  );
+}
